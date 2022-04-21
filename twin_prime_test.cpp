0c314f68f074ee3246eac38871cd3893204ba7f6
Michael Bokatius <64041568+MichaelBokatius@users.noreply.github.com>
2022-04-21 12:33:24 +0200

    Update twin_prime_test.cpp

--- twin_prime_test.cpp
+++ twin_prime_test.cpp
@@ -1,36 +1,62 @@
-#include<stdio.h> // include stdio.h library
+#include<stdio.h> 
+#include<bits/stdc++.h>
+using namespace std;
+
 bool check_prime(int n);
+int print_digital_root(string n);
+
 
 int main(void)
 {   
 
-    int start = 0;
+    int start = 2;
     int end = 100;
+    int sum;
+    string num;
+    std::vector<int> num_pairs;
     
     for(int i = start; i < end; i++)
     {
         if(check_prime(i) && check_prime(i + 2))
-        {            
+        {           
+            num_pairs.push_back(i);
+            num_pairs.push_back(i + 2);
             printf("{%d, %d}\n", i, i + 2);
             i = i + 1; 
         }                
     }    
-    printf("*-- Finished Counting --* \n");
+    printf("*-- Finished Counting --* \n\n");
+    
+    for(int i = 0; i < num_pairs.size() - 2; i++)
+    {
+        sum = num_pairs.at(i) * num_pairs.at(i + 1);
+        num = to_string(sum); 
+        cout<< print_digital_root(num) <<endl;
+        
+    }
+    printf("*-- Finished Digital Sum --* \n\n");
+    
     return 0;
 }
 
 bool check_prime(int n)
 {
-    if(n == 1)
-    {
-        return false;
-    }
+    if(n == 1) return false;
+    
     for(int i = 2; i < n; i++)
     {
-        if(n % i == 0)
-        {
-            return false;
-        }
+        if(n % i == 0) return false;
     }
     return true;
+}
+
+int print_digital_root(string num)
+{
+    if (num.compare("0") == 0) return 0;
+ 
+    int ans = 0;
+    for (int i=0; i<num.length(); i++)
+        ans = (ans + num[i]-'0') % 9;
+ 
+    return (ans == 0)? 9 : ans % 9;
 }
